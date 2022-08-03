e58ab5c8a4e4dacb1d7b73da48557466dfc48cf8
Pedro Barão <phbarao@gmail.com>
2022-08-02 22:21:05 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import { GetStaticProps } from 'next';
+import { getUsers } from '../libs/users';
+
+export default function Home() {
+  function fetchUsers() {
+    fetch('/api/users')
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
+  return <button onClick={fetchUsers}>Buscar</button>;
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const users = await getUsers();
+
+  return {
+    props: { users },
+    revalidate: 5,
+  };
+};
